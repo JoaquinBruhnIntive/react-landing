d6d02f498dc061389e665139ec41b584fa438a6a
JoaquinBruhnIntive <joaquin.bruhn@intive.com>
2021-11-12 17:36:57 -0300

    [main update] finished the Footer component along with it responsive styling, home page is now done

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import logo from '../../assets/logo.svg'
 import { Link } from 'react-router-dom';
+
+import './footer.css'
 
 const Footer = () => {
     return(
-        <footer>
-            <Link to="/">Arch</Link>
-            <Link to="/portfolio">Portfolio</Link>
-            <Link to="/aboutUs">About Us</Link>
-            <Link to="/contact">Contact</Link>
+        <footer className="footer">
+            <div>
+                <Link to="/"><img class="logo" src={logo} alt="logo" /></Link>
+            </div>
+            <div>
+                <Link to="/portfolio">portfolio</Link>
+                <Link to="/aboutUs">about us</Link>
+                <Link to="/contact">contact</Link>
+                <Link to="/portfolio">see our portfolio</Link>
+            </div>
         </footer>
     );
 }
