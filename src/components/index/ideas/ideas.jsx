5b84fff01e67038d08bbc4fe76c8a7c831c464ba
JoaquinBruhnIntive <joaquin.bruhn@intive.com>
2021-11-12 11:04:11 -0300

    [main update] finished the Ideas component with styling and responsiveness and made some minor corrections to styling on other CSS files

--- src/components/index/ideas/ideas.jsx
+++ src/components/index/ideas/ideas.jsx
@@ -1,6 +1,16 @@
+import { Link } from 'react-router-dom';
+import arrow from '../../../assets/white_arrow.png'
+
+import './ideas.css';
+
 const Ideas = () => {
     return (
-        <h2>Ideas</h2>
+        <section className="ideas">
+            <div>
+                <h2>Small team, big ideas</h2>
+                <Link to="/aboutUs">About Us <img src={arrow} alt="right arrow" /></Link>
+            </div>
+        </section>
     )
 }
 
