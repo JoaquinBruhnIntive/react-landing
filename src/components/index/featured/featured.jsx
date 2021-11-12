4af9f63c9f407bb9e958e1c2ceda53034140add9
JoaquinBruhnIntive <joaquin.bruhn@intive.com>
2021-11-12 16:48:19 -0300

    [main update] completed the Featured component on the index page algon with it´s styling

--- src/components/index/featured/featured.jsx
+++ src/components/index/featured/featured.jsx
@@ -1,6 +1,41 @@
+import { Link } from 'react-router-dom';
+import arrow from '../../../assets/white_arrow.png'
+
+import './featured.css'
+
+
+
 const Featured = () => {
     return (
-        <h2>Featured</h2>
+        <section className="featured">
+            <div>
+                <h2>Featured</h2>
+                <Link to="/">See All <img src={arrow} alt="arrow right" /></Link>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <h3>project del sol</h3>
+                        <p>view all projects</p>
+                        <div>1</div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <h3>228B tower</h3>
+                        <p>view all projects</p>
+                        <div>2</div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <h3>le prototipe</h3>
+                        <p>view all proyects</p>
+                        <div>3</div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
