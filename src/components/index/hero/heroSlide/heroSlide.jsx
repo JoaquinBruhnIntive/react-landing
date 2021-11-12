d121026d804310f05f9bfc718de982749458742e
JoaquinBruhnIntive <joaquin.bruhn@intive.com>
2021-11-11 21:02:43 -0300

    [main update] Finished the Welcome component with responsiveness implemented, also did some minor fixes to some of the CSS files of hero.css, hero.slider, app.css, globalStyle.css and added the fonts import to index.html

--- src/components/index/hero/heroSlide/heroSlide.jsx
+++ src/components/index/hero/heroSlide/heroSlide.jsx
@@ -12,18 +12,20 @@ const HeroSlide = ({ slideData }) => {
 
 
     return (
-        <div className={slideData[currentSlide].class}>
-            <div className="hero-content">
-                <h2>Project</h2>
-                <h2>Paramount</h2>
-                <p>
-                    project made for an art museum near Southwest London. Project Paramour is a statement of bold, modern architecture.
-                </p>
-                <Link to="/portfolio">see our portfolio</Link>
+        <div className="slide-opacity">
+            <div className={slideData[currentSlide].class}>
+                <div className="hero-content">
+                    <h2>Project</h2>
+                    <h2>Paramount</h2>
+                    <p>
+                        Project made for an art museum near Southwest London. Project Paramour is a statement of bold, modern architecture.
+                    </p>
+                    <Link to="/portfolio">see our portfolio</Link>
+                </div>
+            <div className="buttonController">
+                {slideData.map((slide, index) => <button onClick={() => toggleSlide(index)}>0{index + 1}</button>)}
+            </div> 
             </div>
-          <div className="buttonController">
-              {slideData.map((slide, index) => <button onClick={() => toggleSlide(index)}>0{index + 1}</button>)}
-          </div> 
         </div>
     );
 }
--- src/components/index/welcome/welcome.jsx
+++ src/components/index/welcome/welcome.jsx
@@ -1,15 +1,21 @@
+import './welcome.css';
+
 const Welcome = () => {
     return (
-        <section>
+        <section className="welcome">
+            <h2>Welcome</h2>
             <div>
-                <h3>Welcome to Arch Studiov</h3>
+                <h3>Welcome to Arch Studio</h3>
                 <p>
                     Welcome to Arch Studio We have a unique network and skillset to help bring your projects to life. Our small team of highly skilled individuals combined with our large network put us in a strong position to deliver exceptional results.
+                </p>
+                <p>
                     Over the past 10 years, we have worked on all kinds of projects. From stations to high-rise buildings, we create spaces that inspire and delight.
+                </p>
+                <p>
                     We work closely with our clients so that we understand the intricacies of each project. This allows us to work in harmony the surrounding area to create truly stunning projects that will stand the test of time.
                 </p>
             </div>
-            <img src="" alt="" />
         </section>
     )
 }
